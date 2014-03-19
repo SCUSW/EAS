73c2c122c2d5fd202a90a04d7dfc9fbe72286381
ccmhwy <ccmhwy@sina.cn>
2014-03-19 19:30:06 +0800

    `

--- src/com/scusw/student/dao/implementation/StudentDaoImpl.java
+++ src/com/scusw/student/dao/implementation/StudentDaoImpl.java
@@ -29,9 +29,7 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：学生通过学号查询自己的信息
-	 * 
-	 * @param studentNo
-	 *            ：学生学号
+	 * @param studentNo ：学生学号
 	 * @return studentInfo：包含学生全部信息的实体
 	 */
 	public StudentInfo queryStudentByNo(String studentNo) {
@@ -44,9 +42,7 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：学生修改自己的信息
-	 * 
-	 * @param student
-	 *            ：包含学生全部信息的实体
+	 * @param student ：包含学生全部信息的实体
 	 */
 	public void updateStudent(StudentInfo student) {
 		this.getHibernateTemplate().update(student);
@@ -54,9 +50,7 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：学生报名
-	 * 
-	 * @param student
-	 *            ：包含学生全部信息的实体
+	 * @param student ：包含学生全部信息的实体
 	 * @return major：学生报名专业所有信息的实体
 	 */
 	public MajorInfo apply(StudentInfo student) {
@@ -70,25 +64,20 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：学生查询公告信息
-	 * 
 	 * @return notice：包含公告信息的实体的集合
 	 */
 	public List<NoticeInfo> noticeInfoQuery() {
-		List<NoticeInfo> notice = getHibernateTemplate()
-				.find("from NoticeInfo");
+		List<NoticeInfo> notice = getHibernateTemplate().find("from NoticeInfo");
 		return notice;
 	}
 
 	/**
 	 * 方法描述：通过公告ID查看公告内容
-	 * 
-	 * @param noticeId
-	 *            公告ID
+	 * @param noticeId : 公告ID
 	 * @return 公告内容
 	 */
 	public String queryNoticeContent(int noticeId) {
-		Query q = this.getSession().createQuery(
-				"from NoticeInfo n where n.noticeId=:noticeId");
+		Query q = this.getSession().createQuery("from NoticeInfo n where n.noticeId=:noticeId");
 		q.setParameter("noticeId", noticeId);
 		NoticeInfo notice = (NoticeInfo) q.uniqueResult();
 		return notice.getNoticeContent();
@@ -96,15 +85,12 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：学生查询专业信息
-	 * 
-	 * @param majorName
-	 *            ：专业名
+	 * @param majorName ：专业名
 	 * @return major：包含专业信息的实体
 	 */
 	public MajorInfo majorInfoQuery(String studentNo) {
 		StudentInfo studentInfo = queryStudentByNo(studentNo);
-		Query q = this.getSession().createQuery(
-				"from MajorInfo m where m.majorName=:majorName");
+		Query q = this.getSession().createQuery("from MajorInfo m where m.majorName=:majorName");
 		q.setParameter("majorName", studentInfo.getStudentMajor());
 		MajorInfo major = (MajorInfo) q.uniqueResult();
 		return major;
@@ -112,9 +98,7 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：学生查询所选课程
-	 * 
-	 * @param studentNo
-	 *            ：学号
+	 * @param studentNo ：学号
 	 * @return ：包含课程信息和选课信息的List对象
 	 */
 	public List<Object[]> queryCourse(String studentNo) {
@@ -127,15 +111,12 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：通过课程编号查询课程
-	 * 
-	 * @param courseId
-	 *            ：所有被查询课程编号
+	 * @param courseId ：所有被查询课程编号
 	 * @return ：查询到的课程集合
 	 */
 	public List<CourseInfo> queryClass(int[] courseId) {
 		List<CourseInfo> course = new ArrayList<CourseInfo>();
-		Query q1 = this.getSession().createQuery(
-				"from CourseInfo c where c.courseId=:courseId");
+		Query q1 = this.getSession().createQuery("from CourseInfo c where c.courseId=:courseId");
 		for (int i = 0; i < courseId.length; i++) {
 			q1.setParameter("courseId", courseId[i]);
 			course.add(((CourseInfo) q1.uniqueResult()));
@@ -145,35 +126,28 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：查询所有课程
-	 * 
 	 * @return ：包含所有课程信息的实体的集合
 	 */
 	public List<CourseInfo> queryAllCourse() {
-		List<CourseInfo> courseInfo = getHibernateTemplate().find(
-				"from CourseInfo c where c.courseAvai=?", 1);
+		List<CourseInfo> courseInfo = getHibernateTemplate().find("from CourseInfo c where c.courseAvai=?", 1);
 		return courseInfo;
 	}
 
 	/**
 	 * 方法描述：查询上课地点
-	 * 
-	 * @param courseInfo
-	 *            ：所有被查询课程
+	 * @param courseInfo ：所有被查询课程
 	 * @return ：所有课程的所有上课地点
 	 */
 	public String[][] queryClassroom(List<CourseInfo> courseInfo) {
 		int size = courseInfo.size();
 		String[][] classroom = new String[size][];
-		Query q = this.getSession().createQuery(
-				"from ClassroomInfo c where c.classroomId=:classroomId");
+		Query q = this.getSession().createQuery("from ClassroomInfo c where c.classroomId=:classroomId");
 		for (int i = 0; i < size; i++) {
 			List<CourseClasshour> courseClasshour = getHibernateTemplate()
-					.find("from CourseClasshour c  where c.courseInfo.courseId =?",
-							courseInfo.get(i).getCourseId());
+					.find("from CourseClasshour c  where c.courseInfo.courseId =?",courseInfo.get(i).getCourseId());
 			classroom[i] = new String[courseClasshour.size()];
 			for (int j = 0; j < courseClasshour.size(); j++) {
-				q.setParameter("classroomId", courseClasshour.get(j)
-						.getClassroomInfo().getClassroomId());
+				q.setParameter("classroomId", courseClasshour.get(j).getClassroomInfo().getClassroomId());
 				ClassroomInfo c = (ClassroomInfo) q.uniqueResult();
 				classroom[i][j] = c.getClassroomAddr();
 			}
@@ -183,26 +157,20 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：查询上课时间
-	 * 
-	 * @param courseInfo
-	 *            ：所有被查询课程
+	 * @param courseInfo ：所有被查询课程
 	 * @return ：所有课程的所有上课时间
 	 */
 	public String[][] queryClasshour(List<CourseInfo> courseInfo) {
 		int size = courseInfo.size();
 		String[][] classhourEveryWeek = new String[size][];
-		Query q = this.getSession().createQuery(
-				"from ClasshourInfo c where c.classhourId=:classhourId");
+		Query q = this.getSession().createQuery("from ClasshourInfo c where c.classhourId=:classhourId");
 		for (int i = 0; i < size; i++) {
 			List<CourseClasshour> courseClasshour = getHibernateTemplate()
-					.find("from CourseClasshour c  where c.courseInfo.courseId =?",
-							courseInfo.get(i).getCourseId());
+					.find("from CourseClasshour c  where c.courseInfo.courseId =?",courseInfo.get(i).getCourseId());
 			classhourEveryWeek[i] = new String[courseClasshour.size()];
 			for (int j = 0; j < courseClasshour.size(); j++) {
-				q.setParameter("classhourId", courseClasshour.get(j)
-						.getClasshourInfo().getClasshourId());
-				classhourEveryWeek[i][j] = ((ClasshourInfo) q.uniqueResult())
-						.getClasshourName();
+				q.setParameter("classhourId", courseClasshour.get(j).getClasshourInfo().getClasshourId());
+				classhourEveryWeek[i][j] = ((ClasshourInfo) q.uniqueResult()).getClasshourName();
 			}
 		}
 		return classhourEveryWeek;
@@ -210,21 +178,17 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：查询课程课时信息编号
-	 * 
-	 * @param course
-	 *            ：所有被查询课程
+	 * @param course ：所有被查询课程
 	 * @return ：被查询课程的所有课程课时编号
 	 */
 	public int[][] queryCourseClasshourId(List<CourseInfo> course) {
 		int[][] courseClasshourId = new int[course.size()][];
 		for (int i = 0; i < course.size(); i++) {
 			List<CourseClasshour> courseClasshour = getHibernateTemplate()
-					.find("from CourseClasshour c  where c.courseInfo.courseId =?",
-							course.get(i).getCourseId());
+					.find("from CourseClasshour c  where c.courseInfo.courseId =?",course.get(i).getCourseId());
 			courseClasshourId[i] = new int[courseClasshour.size()];
 			for (int j = 0; j < courseClasshour.size(); j++) {
-				courseClasshourId[i][j] = courseClasshour.get(j)
-						.getClasshourInfo().getClasshourId();
+				courseClasshourId[i][j] = courseClasshour.get(j).getClasshourInfo().getClasshourId();
 			}
 		}
 		return courseClasshourId;
@@ -232,9 +196,7 @@ public class StudentDaoImpl extends HibernateDaoSupport implements StudentDao {
 
 	/**
 	 * 方法描述：添加所选课程
-	 * 
-	 * @param registerInfos
-	 *            选课信息
+	 * @param registerInfos : 选课信息
 	 */
 	public void addRegisterInfo(List<RegisterInfo> registerInfos) {
 		for (int i = 0; i < registerInfos.size(); i++) {
